b9b2d0dee911f1903fd54cf6876326d7c29b14ba
Eduardo Evaristo <eduardo.evaristo.am@gmail.com>
2025-03-27 01:33:05 -0300

    feat: implement GestureDetector for Images and selection of options

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:pedrapapeltesoura/placar_column.dart';
@@ -7,13 +8,36 @@ void main() {
   runApp(MainApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-  // Does not work work for files located in assets, gotta use Image.asset or AssetImage
-  //final File pedra = File('assets/images/pedra.png');
+class MainApp extends StatefulWidget {
+  MainApp({super.key});
 
-  // Avoiding repetition
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  // Does not work work for files located in assets, gotta use Image.asset or AssetImage
+  List<int> options = [1, 2, 3];
   final String assetPath = 'assets/images';
+  int _currentUserOption = 1;
+  int _currentCpuOption = 1;
+
+  void _cpuSelectsOption() {
+    setState(() {
+      _currentCpuOption = Random().nextInt(3) + 1;
+    });
+  }
+
+  void _userSelectsOption(int option) {
+    setState(() {
+      _currentUserOption = option;
+    });
+  }
+
+  void play(int userOption) {
+    _userSelectsOption(userOption);
+    _cpuSelectsOption();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,10 +49,16 @@ class MainApp extends StatelessWidget {
             Text('Disputa'),
             Row(
               children: <Widget>[
-                Image(image: AssetImage('$assetPath/pedra.png'), width: 100),
+                Image(
+                  image: AssetImage('$assetPath/$_currentUserOption.png'),
+                  width: 100,
+                ),
                 // Image(image: AssetImage('assets/images/papel.png'), width: 100),
                 Text('VS'),
-                Image(image: AssetImage('$assetPath/tesoura.png'), width: 100),
+                Image(
+                  image: AssetImage('$assetPath/$_currentCpuOption.png'),
+                  width: 100,
+                ),
               ],
             ),
             Text('Placar'),
@@ -41,11 +71,18 @@ class MainApp extends StatelessWidget {
             ),
             Text('Opções'),
             Row(
-              children: [
-                Image(image: AssetImage('$assetPath/pedra.png'), width: 100),
-                Image(image: AssetImage('$assetPath/papel.png'), width: 100),
-                Image(image: AssetImage('$assetPath/tesoura.png'), width: 100),
-              ],
+              children:
+                  options
+                      .map<Widget>(
+                        (int i) => GestureDetector(
+                          onTap: () => play(i),
+                          child: Image(
+                            image: AssetImage('$assetPath/$i.png'),
+                            width: 100,
+                          ),
+                        ),
+                      )
+                      .toList(),
             ),
           ],
         ),
